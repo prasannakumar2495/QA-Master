eb8226e3b9209ffa408631a73fa31e9d61d75183
prasannakumar2495 <prasannakumar2461995@gmail.com>
2018-12-11 00:07:32 +0530

    adding the python code with python 3.7 interpretor

--- pythonPractice/samplePractice/firstPython.py
+++ pythonPractice/samplePractice/firstPython.py
@@ -1,14 +1,15 @@
 '''
-Created on 09-Dec-2018
+Created on 11-Dec-2018
 
 @author: prasannakumar
 '''
 #https://www.youtube.com/watch?v=rfscVS0vtbw
 #this is the URL for the python class that lasts for 4:30 HRS
-from StdSuites.AppleScript_Suite import string
-from idlelib.ReplaceDialog import replace
-from __builtin__ import str
+#from StdSuites.AppleScript_Suite import string
+#from idlelib.ReplaceDialog import replace
+#from __builtin__ import str
 from math import *
+
 
 print('hello world')
 
@@ -25,12 +26,12 @@ integer_value2 = str(30)
 print(integer_value2 + " "+ string_value)
 
 #user can even do the concatination by backtick(`)
-print(`integer_value`+ " "+string_value)
+print(str(integer_value)+" "+string_value)
 
 #the variables in python are static(i.e: they always take the lines of code that is written after updating the value of the variable will be displayed.)
 
 string_value ='my name is prasanna kumar'
-print(`integer_value` + string_value)
+print(str(integer_value) + string_value)
 
 #in python "\" is an escape character (this will be working for the special characters that are mentioned after he backslash)
 
@@ -53,6 +54,9 @@ print("the round off of the number---> "+str(round(234.231)))
 
 print(floor(3.9))
 print(ceil(9.1))
+
+my_name = input("enter your name:")
+print("hello "+ my_name)
 
 
 
