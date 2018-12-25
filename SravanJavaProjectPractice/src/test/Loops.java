045f44888efd4f6c10cb54378dc66c70e8b5da81
prasannakumar2495 <prasannakumar2461995@gmail.com>
2018-12-25 10:30:38 +0530

    added the project of sravan

---
+++ SravanJavaProjectPractice/src/test/Loops.java
@@ -0,0 +1,19 @@
+package test;
+
+
+public  class Loops  {
+	final int a=10;
+    {
+	System.out.println("Instance balck from Parent");
+	}
+    
+    Loops()
+    {
+	System.out.println("Con from Parent");
+    }
+    static 
+    {
+    	System.out.println("Static from parent");
+    }
+
+}
