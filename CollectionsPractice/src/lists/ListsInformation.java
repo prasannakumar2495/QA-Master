856b50f05a7addf3412c811b9d51f72bc99f4c46
prasannakumar2495 <prasannakumar2461995@gmail.com>
2018-12-27 15:38:37 +0530

    adding the arrayList code

---
+++ CollectionsPractice/src/lists/ListsInformation.java
@@ -0,0 +1,16 @@
+package lists;
+
+public class ListsInformation 
+{
+/*list is an interface.
+ * list are classified into three types.
+linked list, array list and vector
+
+Array List:
+it an implementation of list interface and user can add and remove the element dynamically.
+the size of the array list can be increased if any new element is added than the initial size.
+
+*/	
+	
+
+}
