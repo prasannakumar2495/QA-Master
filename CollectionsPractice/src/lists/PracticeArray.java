81597c1c404825e7e0a87c046b8ca456a7a493fd
prasannakumar2495 <prasannakumar2461995@gmail.com>
2018-12-27 22:36:02 +0530

    adding the new code changes in the array practice

--- CollectionsPractice/src/lists/PracticeArray.java
+++ CollectionsPractice/src/lists/PracticeArray.java
@@ -1,7 +1,6 @@
 package lists;
 
-public class PracticeArray 
-{
+public class PracticeArray {
 	public void practice() {
 		String[] number = new String[5];
 		number[0] = "dfgvfg";
@@ -9,27 +8,38 @@ public class PracticeArray
 		number[2] = "641";
 		number[3] = "9874198";
 		number[4] = "9874189";
-		//number[5] = "8941";
-		for(int i= 0; i<5 ; i++)
-		{
-		System.out.println("number "+ "is " +number[i]);
+		// number[5] = "8941";
+		for (int i = 0; i < 5; i++) {
+			System.out.println("number " + "is " + number[i]);
 		}
-		
 	}
+
 	private void practice1() {
-		int[] name = new int[] {0,1,2,3,4};
-		for(int i= 0;i<6;i++)
-		{
-			System.out.println(name[i]);	
+		int[] name = new int[] { 0, 1, 2, 3, 4 };
+		for (int i = 0; i < 5; i++) {
+			System.out.println(name[i]);
 		}
-		
-		
 	}
-	public static void main(String[] args) 
-	{
+
+	private String max(String[] name) {
+		String testname = name[0];
+		for (int i = 0; i < 4; i++) {
+			String temp = name[i];
+			if(temp.length()>testname.length())
+			{
+				testname = temp;
+			}
+			
+		}
+		return testname;
+	}
+
+	public static void main(String[] args) {
 		PracticeArray PA = new PracticeArray();
 		PA.practice();
 		PA.practice1();
+		String[] name = { "dfg", "gferg", "ergergvt", "ergthtrhtbtbtr" };
+		String lname = PA.max(name);
+		System.out.println("the longest string is "+lname);
 	}
-
 }
