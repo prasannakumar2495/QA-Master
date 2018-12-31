b9e9024e4d4daef9d52870931b022814392a61a6
prasannakumar2495 <prasannakumar2461995@gmail.com>
2018-12-31 20:22:13 +0530

    adding the List_iterator code

--- CollectionsPractice/src/lists/practiceIterator.java
+++ CollectionsPractice/src/lists/practiceIterator.java
@@ -1,11 +1,12 @@
 package lists;
 
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Vector;
 
 public class practiceIterator {
 	public static void main(String[] args) {
-		Vector<String> v = new Vector(3, 3);
+		Vector<String> v = new Vector();
 		v.addElement("prasanna");
 		v.addElement("kumar");
 		v.addElement("wrefrewf");
@@ -16,19 +17,28 @@ public class practiceIterator {
 		v.addElement("asd");
 		v.addElement("sdf");
 		v.addElement("zxc");
-		
-		//all the methods of an iterator can be used only within the iterator, but not outside.
+
+		// all the methods of an iterator can be used only within the iterator, but not
+		// outside.
 		Iterator<String> it = v.iterator();
-		while(it.hasNext())
-		{
+		while (it.hasNext()) {
 			String value = it.next();
 			System.out.println(value);
-			
-			if(value == "kumar")
-			{
+
+			if (value == "kumar") {
 				break;
 			}
 		}
-		
+		ListIterator<String> lit = v.listIterator();
+		while (lit.hasNext()) {
+			String value1 = lit.next();
+			
+			if (value1.equalsIgnoreCase("wrefrewf")) {
+				lit.remove();
+			}
+			else {
+				System.out.println(value1);
+			}
+		}
 	}
 }
