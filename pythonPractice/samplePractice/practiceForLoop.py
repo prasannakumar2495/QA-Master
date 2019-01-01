bb004871b979b1abf0f5e282eaffc67a0f9eae49
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-01-01 22:31:06 +0530

    adding the for loop code in python

---
+++ pythonPractice/samplePractice/practiceForLoop.py
@@ -0,0 +1,18 @@
+'''
+Created on 01-Jan-2019
+
+@author: prasannakumar
+'''
+friends = ['prasanna', 'kumar', 'dj']
+
+for loop in 'prasanna kumar':
+    print(loop)
+    for friend in friends:
+        print(friend)
+        
+for numbers in range(3,10):
+    print(numbers)
+
+for index in range(len(friends)):
+    print(index)
+    print(friends[index])
