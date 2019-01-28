e9440f5a0acf55503aa7e605b15d6e674b834f37
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-01-28 11:37:40 +0530

    adding the code

---
+++ Test_pk/src/practice/Test.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class Test {
+	
+	int A = 10;
+	static int b = 23;
+	
+	private void rest() {
+		int local = 234;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int A = 12;
+		int b = 123;
+		
+		System.out.println(A);
+		System.out.println(b);
+		System.out.println(A);
+		System.out.println(A);
+	}
+
+}
