3005bcf82cbbe0623f29d6698f671d86d81de131
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-02-14 14:56:55 +0530

    adding the new code

---
+++ com.maven.test1/Features/test.feature
@@ -0,0 +1,6 @@
+Feature: test the test
+
+Scenario: rest test
+    Given sgrg segersg
+    When hbrthr ehrth erg
+    Then rhtr erer eryer e5y e5y e5
--- com.practiceAny.maven/src/test/java/com/practiceAny/maven/RetriveData.java
+++ com.practiceAny.maven/src/test/java/com/practiceAny/maven/RetriveData.java
@@ -3,42 +3,47 @@ package com.practiceAny.maven;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.util.CellReference;
 import org.testng.annotations.Test;
 
 public class RetriveData {
-	
+
 	@Test
 	private void retrivedata() throws IOException {
-		FileInputStream sampleFile = new FileInputStream("D:\\GIT\\QA-Master\\com.practiceAny.maven\\SampleXLSFile_212kb.xls");
-		HSSFWorkbook  wb = new HSSFWorkbook(sampleFile);
-        HSSFWorkbook test = new HSSFWorkbook(); 
-        HSSFSheet sheet = wb.getSheet("PK");
-        HSSFRow row; 
-        HSSFCell cell;
-        Iterator rows = sheet.rowIterator();
-       /* while (rows.hasNext())
-        {
-            row=(HSSFRow) rows.next();
-            Iterator cells = row.cellIterator();
-            while (cells.hasNext())
-            {
-                cell=(HSSFCell) cells.next();   
-                if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING)
-                {
-                    System.out.print(cell.getStringCellValue()+" ");
-                }
-                else if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC)
-                {
-                    System.out.print(cell.getNumericCellValue()+" ");
-                }
-                else
-                {
-                }
-            }
-            System.out.println();
-        }*/
-    }   
+		FileInputStream sampleFile = new FileInputStream(
+				"D:\\GIT\\QA-Master\\com.practiceAny.maven\\SampleXLSFile_212kb.xls");
+		HSSFWorkbook wb = new HSSFWorkbook(sampleFile);
+		// HSSFWorkbook test = new HSSFWorkbook();
+		HSSFSheet sheet = wb.getSheetAt(0);
+		HSSFRow row = null;
+		HSSFCell cell;
+		// Making the object of excel row
+		row = sheet.getRow(0);
+		int lstclmn = row.getLastCellNum();
+		int lstrow = sheet.getLastRowNum();
+
+		// Iterator rows = sheet.rowIterator();
+		/*
+		 * while (rows.hasNext()) { row=(HSSFRow) rows.next(); Iterator cells =
+		 * row.cellIterator(); while (cells.hasNext()) { cell=(HSSFCell) cells.next();
+		 * if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
+		 * System.out.println(cell.getStringCellValue()+" "); } else
+		 * if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
+		 * System.out.println(cell.getNumericCellValue()+" "); } else { }
+		 * 
+		 * cell=(HSSFCell) cells.next(); try {
+		 * System.out.print(cell.getStringCellValue()+" "); } catch (Exception e) { //
+		 * TODO Auto-generated catch block e.printStackTrace(); System.out.println(e); }
+		 * } System.out.println(); }
+		 */
+		// LinkedList<Integer> i = new LinkedList();
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j <= lstrow; j++) {
+				System.out.println(wb.getSheetAt(i).getRow(j).getCell(CellReference.convertColStringToIndex("B")));
+			}
+		}
+	}
 }
-	
