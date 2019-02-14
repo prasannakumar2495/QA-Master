f24951ab1e85e6c65384dae2801a014a41729fe9
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-02-14 19:36:18 +0530

    adding the new code

--- com.practiceAny.maven/src/test/java/com/practiceAny/maven/RetriveData.java
+++ com.practiceAny.maven/src/test/java/com/practiceAny/maven/RetriveData.java
@@ -1,6 +1,8 @@
 package com.practiceAny.maven;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -10,20 +12,15 @@ import org.apache.poi.hssf.util.CellReference;
 import org.testng.annotations.Test;
 
 public class RetriveData {
-
-	@Test
+	public static int count = 0;
+	@Test(invocationCount = 10)
 	private void retrivedata() throws IOException {
+		//int count = 0;
 		FileInputStream sampleFile = new FileInputStream(
 				"D:\\GIT\\QA-Master\\com.practiceAny.maven\\SampleXLSFile_212kb.xls");
 		HSSFWorkbook wb = new HSSFWorkbook(sampleFile);
 		// HSSFWorkbook test = new HSSFWorkbook();
-		HSSFSheet sheet = wb.getSheetAt(0);
-		HSSFRow row = null;
-		HSSFCell cell;
-		// Making the object of excel row
-		row = sheet.getRow(0);
-		int lstclmn = row.getLastCellNum();
-		int lstrow = sheet.getLastRowNum();
+		
 
 		// Iterator rows = sheet.rowIterator();
 		/*
@@ -40,10 +37,34 @@ public class RetriveData {
 		 * } System.out.println(); }
 		 */
 		// LinkedList<Integer> i = new LinkedList();
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i <= 1; i++) {
+			HSSFSheet sheet = wb.getSheetAt(i);
+			HSSFRow row = null;
+			HSSFCell cell;
+			// Making the object of excel row
+			row = sheet.getRow(0);
+			int lstclmn = row.getLastCellNum();
+			int lstrow = sheet.getLastRowNum();
 			for (int j = 0; j <= lstrow; j++) {
+				
 				System.out.println(wb.getSheetAt(i).getRow(j).getCell(CellReference.convertColStringToIndex("B")));
-			}
+				
+			}System.out.println(lstclmn+" "+lstrow);
 		}
+	//count = count++;
+		System.out.println((count++) + 1);
+		//System.out.println(lstclmn+" "+lstrow);
+		/*row.createCell(1).setCellValue("pksdgsg");
+		sampleFile.close();
+		FileOutputStream outFile =new FileOutputStream(new File("D:\\GIT\\QA-Master\\com.practiceAny.maven\\SampleXLSFile_212kb.xls"));
+		wb.write(outFile);
+        outFile.close();*/
+		
 	}
+	/*@Test
+	public void fcount()
+	{
+		System.out.println(count);
+	}
+*/
 }
