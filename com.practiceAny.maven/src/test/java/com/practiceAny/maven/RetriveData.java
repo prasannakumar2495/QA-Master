b9769720a75b47e7702e16c7c33d19ce894dd119
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-02-15 14:28:37 +0530

    adding the new code

--- com.practiceAny.maven/src/test/java/com/practiceAny/maven/RetriveData.java
+++ com.practiceAny.maven/src/test/java/com/practiceAny/maven/RetriveData.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.hssf.util.CellReference;
@@ -13,58 +11,38 @@ import org.testng.annotations.Test;
 
 public class RetriveData {
 	public static int count = 0;
-	@Test(invocationCount = 10)
+
+	@Test(invocationCount = 1)
 	private void retrivedata() throws IOException {
-		//int count = 0;
 		FileInputStream sampleFile = new FileInputStream(
 				"D:\\GIT\\QA-Master\\com.practiceAny.maven\\SampleXLSFile_212kb.xls");
 		HSSFWorkbook wb = new HSSFWorkbook(sampleFile);
-		// HSSFWorkbook test = new HSSFWorkbook();
-		
 
-		// Iterator rows = sheet.rowIterator();
-		/*
-		 * while (rows.hasNext()) { row=(HSSFRow) rows.next(); Iterator cells =
-		 * row.cellIterator(); while (cells.hasNext()) { cell=(HSSFCell) cells.next();
-		 * if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
-		 * System.out.println(cell.getStringCellValue()+" "); } else
-		 * if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
-		 * System.out.println(cell.getNumericCellValue()+" "); } else { }
-		 * 
-		 * cell=(HSSFCell) cells.next(); try {
-		 * System.out.print(cell.getStringCellValue()+" "); } catch (Exception e) { //
-		 * TODO Auto-generated catch block e.printStackTrace(); System.out.println(e); }
-		 * } System.out.println(); }
-		 */
-		// LinkedList<Integer> i = new LinkedList();
-		for (int i = 0; i <= 1; i++) {
+		for (int i = 0; i < 1; i++) {
 			HSSFSheet sheet = wb.getSheetAt(i);
 			HSSFRow row = null;
 			HSSFCell cell;
 			// Making the object of excel row
 			row = sheet.getRow(0);
+			// from the above row the user can fetch the last cell count in the next line
 			int lstclmn = row.getLastCellNum();
 			int lstrow = sheet.getLastRowNum();
 			for (int j = 0; j <= lstrow; j++) {
-				
-				System.out.println(wb.getSheetAt(i).getRow(j).getCell(CellReference.convertColStringToIndex("B")));
-				
-			}System.out.println(lstclmn+" "+lstrow);
+				HSSFCell c = (HSSFCell) wb.getSheetAt(i).getRow(j).getCell(7);
+				// System.out.println(c);
+				for (int k = 0; k <= lstclmn; k++) {
+
+					// System.out.println(wb.getSheetAt(i).getRow(j).getCell(CellReference.convertColStringToIndex(k)));
+					HSSFCell r = wb.getSheetAt(i).getRow(j).getCell(k);
+
+					System.out.println(r + " contains " + c);
+				}
+
+			}
+			System.out.println(lstclmn + " " + (lstrow+1));
 		}
-	//count = count++;
+		// count = count++;
 		System.out.println((count++) + 1);
-		//System.out.println(lstclmn+" "+lstrow);
-		/*row.createCell(1).setCellValue("pksdgsg");
-		sampleFile.close();
-		FileOutputStream outFile =new FileOutputStream(new File("D:\\GIT\\QA-Master\\com.practiceAny.maven\\SampleXLSFile_212kb.xls"));
-		wb.write(outFile);
-        outFile.close();*/
-		
+
 	}
-	/*@Test
-	public void fcount()
-	{
-		System.out.println(count);
-	}
-*/
 }
