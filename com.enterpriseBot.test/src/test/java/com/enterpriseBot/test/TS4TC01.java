52964abbe216b524d24e11e81112e0666f95bf08
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-03-04 11:40:14 +0530

    adding the new code

--- com.enterpriseBot.test/src/test/java/com/enterpriseBot/test/TS4TC01.java
+++ com.enterpriseBot.test/src/test/java/com/enterpriseBot/test/TS4TC01.java
@@ -1,5 +1,6 @@
 package com.enterpriseBot.test;
 
+import java.awt.Container;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
@@ -10,9 +11,10 @@ import org.testng.annotations.Test;
 public class TS4TC01 {
 	WebDriver driver;
 	JavascriptExecutor jse;
-	@Test(priority=0)
+
+	@Test(priority = 0)
 	private void dashboard() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "/Users/prasannakumaranisetti/Movies/GIT/QA-Master/com.enterpriseBot.test/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "D:\\GIT\\QA-Master\\com.enterpriseBot.test\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		driver.manage().window().maximize();
@@ -22,33 +24,40 @@ public class TS4TC01 {
 		driver.findElement(By.xpath("//input[@name='usr']")).sendKeys("suraj+tester@enterprisebot.org");
 		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("E13_Tester");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		Thread.sleep(15000);
-		WebElement searchBar=driver.findElement(By.xpath("//*[@id=\"search-form\"]"));
+		Thread.sleep(5000);
+		WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"search-form\"]"));
 		searchBar.sendKeys("best");
 		searchBar.sendKeys(Keys.ENTER);
-		WebElement clicksearchvalue = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='work-area intent-list-div']/div/ul/li/span")));
-		Thread.sleep(10000);
-		//jse.executeScript("arguments[0].click();", clicksearchvalue);
+		WebElement clicksearchvalue = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//div[@class='work-area intent-list-div']/div/ul/li/span")));
+		Thread.sleep(3000);
 		clicksearchvalue.click();
-		Thread.sleep(10000);
+		Thread.sleep(5000);
+
 		String data = "prasanna kumar money5";
-		WebElement AddTrigger=driver.findElement(By.xpath("//input[@placeholder='Add triggers']"));
+		WebElement AddTrigger = driver.findElement(By.xpath("//input[@placeholder='Add triggers']"));
 		AddTrigger.sendKeys(data);
 		AddTrigger.sendKeys(Keys.ENTER);
-		driver.findElement(By.xpath("//button[@class='btn btn-success agent-save-button']")).click();
-		driver.switchTo().alert().dismiss();
-		Thread.sleep(15000);
+		WebElement SAVE = driver
+				.findElement(By.xpath("//button[@class='btn btn-success agent-save-button'][@id='intent-save-btn']"));
+		//jse.executeScript("arguments[0].click();", SAVE);
+		SAVE.click();
+		// driver.switchTo().alert().dismiss();
+		Thread.sleep(5000);
 		driver.findElement(By.xpath("//input[@placeholder='Search triggers...']")).sendKeys("prasanna kumar money5");
-		String options=driver.findElement(By.xpath("//*[@id=\"trigger-section\"]/div[2]/div[2]/ul/li[1]/div/div")).getText();
-		if(data.equalsIgnoreCase(options))
-		{
+		String options = driver.findElement(By.xpath("//*[@id=\"trigger-section\"]/div[2]/div[2]/ul/li[1]/div/div"))
+				.getText();
+		WebElement searchTrigger = driver
+				.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[2]/button[2]"));
+		searchTrigger.sendKeys("prasanna kumar money5");
+		searchTrigger.sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//div[@class='button']/button[1]")).click();
+		if (data.equalsIgnoreCase(options)) {
 			System.out.println("the entered data did  match");
-		}
-		else {
+		} else {
 			System.out.println("the entered data did not match");
-
 		}
-		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[2]/button[2]")).click();
-		driver.quit();
+
+		// driver.quit();
 	}
 }
