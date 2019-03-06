ce6771f63fbcbdaf6b62116d4469b2457476d91f
prasannakumar2495 <prasannakumar2461995@gmail.com>
2019-03-06 18:34:53 +0530

    code is done

--- com.practiceAny.maven/src/test/java/com/practiceAny/maven/Filter2Files.java
+++ com.practiceAny.maven/src/test/java/com/practiceAny/maven/Filter2Files.java
@@ -11,6 +11,7 @@ public class Filter2Files {
 	int count = 0;
 	int countopdata = 0;
 	Cell newcell;
+	Cell newscndcell;
 	int NumberFile;
 	int sizeOfFilter;
 	XSSFSheet ipsheet;
@@ -31,9 +32,6 @@ public class Filter2Files {
 
 	FileOutputStream fos;
 
-	DataFormatter dataFormatter = new DataFormatter();
-	String cellValue;
-
 	// collections list to save all the data of the row numbers that have to be
 	// fetched.
 	List<Integer> allreqrow = new ArrayList<Integer>();
@@ -47,32 +45,22 @@ public class Filter2Files {
 	// collection of all the data from the required rows while reading the files.
 	LinkedList<String> allreqdatafile2 = new LinkedList<String>();
 
-	// compare sheet
-	XSSFRow cmprow;
-	int cmplstclmn;
-	int cmplstrow;
-	int cmpfrstrow;
-
 	@Test(priority = 0)
 	public void eachFile() throws Exception {
 		// adding the path of the properties files
 		fis = new FileInputStream("D:\\GIT\\QA-Master\\com.practiceAny.maven\\Properties\\property2.properties");
 		prop.load(fis);
 
-		// number of the filters to be applied for
-		sizeOfFilter = Integer.parseInt(prop.getProperty("size"));
-
 		// number of files to be run.
 		NumberFile = Integer.parseInt(prop.getProperty("No.OfFiles"));
 
-		for (g = 0; g <= NumberFile; g++) {
+		for (g = 1; g <= NumberFile; g++) {
 			if (g == 1) {
 				System.out.println("entered into the loop equal to 1");
 				// loading the excel files.
 				sampleFile = new FileInputStream(prop.getProperty("inputfile1"));
 				readFile();
 				allreqdatafile1.addAll(allreqdata);
-				System.out.println("the data in the file: " + " is " + allreqdatafile1);
 				allreqdata.clear();
 			} else if (g == 2) {
 				System.out.println("entered into the loop equal to 2");
@@ -80,14 +68,10 @@ public class Filter2Files {
 				sampleFile = new FileInputStream(prop.getProperty("inputfile2"));
 				readFile();
 				allreqdatafile2.addAll(allreqdata);
-				System.out.println("the data in the file: " + " is " + allreqdatafile2);
-				System.out.println("the size of the data from the file number 2 is: "+allreqdatafile2.size());
-				System.out.println("the data in the cell is: "+ allreqdatafile2.get(1));
-				writeFile();
-				System.out.println("the data is being written into the sheet 2");
 				allreqdata.clear();
 			}
 		}
+		compareColumns();
 	}
 
 	@Test(invocationCount = 0)
@@ -104,24 +88,14 @@ public class Filter2Files {
 			lstclmn = row.getLastCellNum();
 			lstrow = ipsheet.getLastRowNum();
 			frtrow = ipsheet.getFirstRowNum();
-
 			// loop for the rows
-			for (int j = 0; j <= lstrow; j++) {
-				data = wb.getSheetAt(i).getRow(j).getCell(1).getStringCellValue();
-
-				// loop for the number of the data to be identified in the excel sheet, so
-				// converting them into the string format in the next line
-				for (int p = 0; p <= sizeOfFilter; p++) {
-					String q = Integer.toString(p);
-
-					// condition to validate the data
-					if (data.equalsIgnoreCase(prop.getProperty(q))) {
-						reqrow = wb.getSheetAt(i).getRow(j).getRowNum();
-						System.out.println("this is the row: " + (reqrow + 1));
-						allreqrow.add((reqrow + 1));
-						allreqdata.add(data);
-					}
+			try {
+				for (int j = 0; j <= lstrow; j++) {
+					data = wb.getSheetAt(i).getRow(j).getCell(0).getStringCellValue();
+					allreqdata.add(data);
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			System.out.println();
 		}
@@ -132,49 +106,19 @@ public class Filter2Files {
 	}
 
 	@Test(invocationCount = 0)
-	public void writeFile() throws Exception {
-		XSSFWorkbook opwb = new XSSFWorkbook();
-		XSSFSheet opsheet = opwb.createSheet("Data to be compared");
-		for (int w = 0; w <= sizeOfFilter; w++) {
-			int numrow = 0;
-			for (String value1 : allreqdatafile1) {
-				numrow++;
-				oprow = opsheet.createRow(numrow);
-				newcell = oprow.createCell(0);
-				newcell.setCellValue(value1);
-			}
-			for (int i = 1; i <= sizeOfFilter + 1; i++) {
-				newcell = opsheet.getRow(i).createCell(1);
-				newcell.setCellValue(allreqdatafile2.get(i-1));
-			}
-		}
-		fos = new FileOutputStream(prop.getProperty("comparedFile"));
-		opwb.write(fos);
-		fos.flush();
-		fos.close();
-		System.out.println("createworkbook.xlsx written successfully");
-
-	}
-
-	@Test(priority = 1, invocationCount = 0)
 	public void compareColumns() throws IOException {
-		FileInputStream fiscmp = new FileInputStream(prop.getProperty("comparedFile"));
-		XSSFWorkbook cmpwb = new XSSFWorkbook(fiscmp);
-		XSSFSheet cmpsheet = cmpwb.getSheet("Data to be compared");
-		cmprow = null;
-		cmprow = cmpsheet.getRow(1);
-		cmplstclmn = cmprow.getLastCellNum();
-		System.out.println("the last column is: " + cmplstclmn);
-		cmplstrow = cmpsheet.getLastRowNum();
-		System.out.println("the last row is: " + cmplstrow);
-		cmpfrstrow = cmpsheet.getFirstRowNum();
-		for (int i = 0; i <= cmplstrow; i++) {
-			String cmpfrstcellvalue = cmpsheet.getRow(i).getCell(0).getStringCellValue();
-			String cmpscndcellvalue = cmpsheet.getRow(i).getCell(1).getStringCellValue();
-			if (cmpfrstcellvalue.equalsIgnoreCase(cmpscndcellvalue)) {
-				System.out.println(cmpfrstcellvalue + " is equal to: " + cmpscndcellvalue);
+		try {
+			for (int i = 0; i < allreqdatafile1.size(); i++) {
+				if (allreqdatafile2.contains(allreqdatafile1.get(i))) {
+					// System.out.println(allreqdatafile1.get(i)+"---> this value is present in both
+					// the files");
+				} else {
+					System.out.println();
+					System.out.println(allreqdatafile1.get(i) + "---> this value is not present in the second file");
+				}
 			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 		}
-		fiscmp.close();
 	}
 }
